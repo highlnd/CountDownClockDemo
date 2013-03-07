782115f0c9094ea7f866b701e91610b41ccaf37b
Mike Kennedy <mkennedy76@gmail.com>
2013-03-06 23:01:30 -0800

    Initial Commit

---
+++ CountDownClockDemo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CountDownClockDemo
+//
+//  Created by Mike Kennedy on 3/6/13.
+//  Copyright (c) 2013 Mike Kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CountDownClockDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CountDownClockDemo
+//
+//  Created by Mike Kennedy on 3/6/13.
+//  Copyright (c) 2013 Mike Kennedy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
